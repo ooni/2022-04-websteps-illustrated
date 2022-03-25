a257c396b178d74c8a6f8a324c130fe326d9ea53
Simone Basso <bassosimone@gmail.com>
2022-03-25 14:03:53 +0100

    refactor(atomicx): make it a ~zero-cost abstraction
    We can reduce the complexity of Int64 by just ensuring its int64 value is
    the first element: this is enough to guarantee alignment.
    
    This allows us to drop the mutex and really reduce the cost of calling int64
    atomic functions. It is not really zero cost but the cost is less.
    
    Also, this solution is more elegant.

--- internal/atomicx/atomicx.go
+++ internal/atomicx/atomicx.go
@@ -11,34 +11,14 @@
 //
 // It is of course possible to write atomic code using 64 bit variables on a
 // 32 bit platform, but that's difficult to do correctly. This package
-// provides an easier-to-use interface. We use allocated
-// structures protected by a mutex that encapsulate a int64 value.
+// provides an easier-to-use interface. We allocate structures so to ensure
+// that 64 bit values are always boundary aligned.
 package atomicx
 
-import (
-	"sync"
-	"sync/atomic"
-)
-
-/*
-	TODO(bassosimone): Go documentation states that:
-
-	  On ARM, 386, and 32-bit MIPS, it is the caller's responsibility
-	  to arrange for 64-bit alignment of 64-bit words accessed atomically. The
-	  first word in a variable or in an allocated struct, array, or slice can
-	  be relied upon to be 64-bit aligned.
-
-	Would this mean that Int64 could use sync/atomic directly if it
-	only contained `v` as its first and unique element?
-
-	See https://pkg.go.dev/sync/atomic#pkg-note-BUG
-*/
+import "sync/atomic"
 
 // Int64 is an int64 with atomic semantics.
 type Int64 struct {
-	// mu provides mutual exclusion.
-	mu sync.Mutex
-
 	// v is the underlying value.
 	v int64
 }
@@ -46,46 +26,21 @@ type Int64 struct {
 // NewInt64 creates a new Int64 with the given value.
 func NewInt64(value int64) *Int64 {
 	return &Int64{
-		mu: sync.Mutex{},
-		v:  value,
+		v: value,
 	}
 }
 
 // Add behaves like atomic.AddInt64.
 func (i64 *Int64) Add(delta int64) int64 {
-	i64.mu.Lock()
-	defer i64.mu.Unlock()
-	i64.v += delta
-	return i64.v
+	return atomic.AddInt64(&i64.v, delta)
 }
 
 // Load behaves like atomic.LoadInt64.
-func (i64 *Int64) Load() (v int64) {
-	return i64.Add(0)
+func (i64 *Int64) Load() int64 {
+	return atomic.LoadInt64(&i64.v)
 }
 
-// Int32 is an int32 with atomic semantics.
-type Int32 struct {
-	// v is the underlying value.
-	v int32
-}
-
-// NewInt32 creates an atomic int32 with the given value.
-func NewInt32(value int32) *Int32 {
-	return &Int32{v: value}
-}
-
-// Add behaves like Int64.Add.
-func (v *Int32) Add(delta int32) int32 {
-	return atomic.AddInt32(&v.v, delta)
-}
-
-// Load behaves like Int64.Load.
-func (v *Int32) Load() int32 {
-	return atomic.LoadInt32(&v.v)
-}
-
-// Swap behaves like atomic.SwapInt32
-func (v *Int32) Swap(other int32) int32 {
-	return atomic.SwapInt32(&v.v, other)
+// Swap behaves like atomic.SwapInt64.
+func (i64 *Int64) Swap(val int64) int64 {
+	return atomic.SwapInt64(&i64.v, val)
 }
